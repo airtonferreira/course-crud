84cbe6aa0d763627c82271d675ca10e0933abc99
airton.felix <airton.felix@dcx.ufpb.br>
2020-01-26 15:17:47 +0000

    hotfixes/form validations

--- src/app/course-form/course-form.component.ts
+++ src/app/course-form/course-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-course-form',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  submitted = false;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.form = this.fb.group({
+      name: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(250)]]
+    });
+
   }
 
+  hasError(field: string) {
+    return this.form.get(field).errors;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.form.valid) {
+      console.log('Form enviado com sucesso!')
+    }
+  }
+
+  onCancel() {
+    this.submitted = false;
+    this.form.reset();
+  }
+ 
 }
