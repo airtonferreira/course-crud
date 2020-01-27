f74303afcde1ba1f178317c01d41eb4aa6cc5714
airton.felix <airton.felix@dcx.ufpb.br>
2020-01-27 02:18:32 +0000

    hotfixes/Resolve added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,31 @@
-import { HomeComponent } from './home/home.component';
-import { CourseFormComponent } from './course-form/course-form.component';
-import { CourseListComponent } from './course-list/course-list.component';
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { CourseResolverGuard } from "./guards/course-resolver.guard";
+import { HomeComponent } from "./home/home.component";
+import { CourseFormComponent } from "./course-form/course-form.component";
+import { CourseListComponent } from "./course-list/course-list.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'courses', component: CourseListComponent },
-  { path: 'new-course', component: CourseFormComponent },
-  { path: 'courses/edit/:id', component: CourseFormComponent }
+  { path: "", component: HomeComponent },
+  { path: "courses", component: CourseListComponent },
+  {
+    path: "new-course",
+    component: CourseFormComponent,
+    resolve: {
+      course: CourseResolverGuard
+    }
+  },
+  {
+    path: "courses/edit/:id",
+    component: CourseFormComponent,
+    resolve: {
+      course: CourseResolverGuard
+    }
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
